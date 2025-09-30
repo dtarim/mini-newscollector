6cb378cbc1949c7319626821fe173aecf48cc727
Didem Benlioglu <didem.benlioglu@gmail.com>
2025-09-30 15:53:37 +0200

    added test code in ci/cd

---
+++ app/test_main.py
@@ -0,0 +1,21 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Mini News Collector is running!"}
+
+def test_refresh_and_articles():
+    # Önce refresh çağır
+    response = client.post("/refresh")
+    assert response.status_code == 200
+
+    # Sonra articles endpointini test et
+    response = client.get("/articles")
+    assert response.status_code == 200
+    data = response.json()
+    assert "articles" in data
+    assert isinstance(data["articles"], list)
